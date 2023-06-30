a1adfb7c813ca97f6666010d7c4aadec374130e4
Yannick Fricke <yannickfricke@googlemail.com>
2023-06-30 05:07:04 +0000

    perf: reduce rerenders
    Using "useCallback" reduces the amount of possible rerenders since it
    caches the given function until one of its dependencies is changing.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import React, { useState, SetStateAction } from 'react';
+import React, { useState, SetStateAction, useCallback } from 'react';
 
 export const useResettableState = <T>(
     initialState: T
 ): [T, () => void, React.Dispatch<SetStateAction<T>>] => {
     const [state, setState] = useState<T>(initialState);
 
-    const resetState = () => setState(initialState);
+    const resetState = useCallback(() => setState(initialState), [initialState])
 
     return [state, resetState, setState];
 };
