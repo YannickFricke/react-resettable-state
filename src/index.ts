f4e0fb04c44c8756de8290b4bd7071e633f236b6
Yannick Fricke <yannickfricke@googlemail.com>
2023-07-06 07:08:27 +0000

    style(index): fixed style according to new prettier
    Somehow prettier is enforcing new rules after an update from 2.8.8 to
    3.0.0...

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import React, { useState, SetStateAction, useCallback } from 'react';
 
 export const useResettableState = <T>(
-    initialState: T
+    initialState: T,
 ): [T, () => void, React.Dispatch<SetStateAction<T>>] => {
     const [state, setState] = useState<T>(initialState);
 
     const resetState = useCallback(
         () => setState(initialState),
-        [initialState]
+        [initialState],
     );
 
     return [state, resetState, setState];
